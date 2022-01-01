efee6242d1ee19d42c19745243ed699d069a8ed6
jamontes <jamontes@users.noreply.github.com>
2022-01-01 21:30:49 +0100

    Code updated for matrix onwards.

--- resources/lib/lutil.py
+++ resources/lib/lutil.py
@@ -1,7 +1,7 @@
 # _*_ coding: utf-8 _*_
 
 '''
-   lutil: library functions for XBMC video plugins.
+   lutil: library functions for KODI video plugins.
    Copyright (C) 2013 José Antonio Montes (jamontes)
 
    This program is free software: you can redistribute it and/or modify
@@ -20,15 +20,22 @@
    Description:
    These funtions are called from the main plugin module, aimed to ease
    and simplify the plugin development process.
-   Release 0.1.4
+   Release 1.3.0
 '''
 
 # First of all We must import all the libraries used for plugin development.
-import sys, re, urllib, urllib2, json, os
+import sys, re, os
+from urllib.parse import urlencode, quote_plus, unquote_plus
+from urllib.request import urlopen, Request
 import xbmcplugin, xbmcaddon, xbmcgui, xbmcaddon, xbmc
 
 debug_enable = False # The debug logs are disabled by default.
 fanart_file = "" # Initialize the global var for the fanart file location.
+
+
+def get_plugin_handle():
+    """This function returns the plugin handle to main module."""
+    return int(sys.argv[1])
 
 
 def get_plugin_settings(plugin_id=""):
@@ -56,23 +63,23 @@ def set_fanart_file(root_path=""):
 
 
 def log(message):
-    """This function logs the messages into the main XBMC log file, only if debug option is activated from the add-on settings.
+    """This function logs the messages into the main KODI log file, only if debug option is activated from the add-on settings.
     This function is called from the main add-on module."""
     if debug_enable:
         try:
-            xbmc.log(msg=message, level=xbmc.LOGNOTICE)
+            xbmc.log(msg=message, level=xbmc.LOGINFO)
         except:
-            xbmc.log(msg='WARNING: log this line from ESO add-on is not possible due to encoding string problems', level=xbmc.LOGNOTICE)
+            xbmc.log(msg='WARNING: log this line from ESO add-on is not possible due to encoding string problems', level=xbmc.LOGINFO)
 
 
 def get_plugin_parms():
-    """This function gets all the parameters passed to the plugin from XBMC API and retuns a dictionary.
+    """This function gets all the parameters passed to the plugin from KODI API and retuns a dictionary.
     Example: plugin://plugin.video.atactv/?parametro1=valor1&parametro2=valor2&parametro3"""
     params = sys.argv[2]
     log("get_plugin_parms " + str(params))
 
     pattern_params  = re.compile('[?&]([^=&]+)=?([^&]*)')
-    options = dict((parameter, urllib.unquote_plus(value)) for (parameter, value) in pattern_params.findall(params))
+    options = dict((parameter, unquote_plus(value)) for (parameter, value) in pattern_params.findall(params))
 
     log("get_plugin_parms " + repr(options))
     return options
@@ -81,13 +88,13 @@ def get_plugin_parms():
 def get_url_decoded(url):
     """This function returns the URL decoded."""
     log('get_url_decoded URL: "%s"' % url)
-    return urllib.unquote_plus(url)
+    return unquote_plus(url)
 
 
 def get_url_encoded(url):
     """This function returns the URL encoded."""
     log('get_url_encoded URL: "%s"' % url)
-    return urllib.quote_plus(url)
+    return quote_plus(url)
 
 
 def set_view_mode(viewid):
@@ -124,11 +131,12 @@ def carga_web(url):
     """This function loads the html code from a webserver and returns it into a string."""
     log('carga_web URL: "%s"' % url)
 
-    MiReq = urllib2.Request(url) # We use the Request method because we need to add a header into the HTTP GET to the web site.
+    MiReq = Request(url) # We use the Request method because we need to add a header into the HTTP GET to the web site.
     # We have to tell the web site we are using a real browser.
     MiReq.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0') # This is a true Firefox header.
-    MiConex = urllib2.urlopen(MiReq) # We open the HTTP connection to the URL.
-    MiHTML = MiConex.read() # We load all the HTML contents from the web page and store it into a var.
+    MiConex = urlopen(MiReq) # We open the HTTP connection to the URL.
+    encoding = MiConex.info().get_param('charset', 'utf8')
+    MiHTML = MiConex.read().decode(encoding, 'ignore') # We load all the HTML contents from the web page and store it into a var.
     MiConex.close() # We close the HTTP connection as we have all the info required.
 
     return MiHTML
@@ -155,29 +163,29 @@ def find_first(text,pattern):
 
 
 def addDir(action = "", title = "", url = "", genre = "", reset_cache = "no"):
-    """This function adds a directory entry into the XBMC GUI throught the API."""
+    """This function adds a directory entry into the KODI GUI throught the API."""
     log('addDir action = "%s" url = "%s" reset_cache = "%s"' % (action, url, reset_cache))
 
-    dir_url = '%s?action=%s&reset_cache=%s&url=%s&genre=%s' % (sys.argv[0], action, reset_cache, urllib.quote_plus(url), urllib.quote_plus(genre))
-    dir_item = xbmcgui.ListItem(title, iconImage = "DefaultFolder.png", thumbnailImage = '')
+    dir_url = '%s?action=%s&reset_cache=%s&url=%s&genre=%s' % (sys.argv[0], action, reset_cache, quote_plus(url), quote_plus(genre))
+    dir_item = xbmcgui.ListItem(title)
     dir_item.setInfo(type = "Video", infoLabels = {"Title": title, "Genre": genre})
-    dir_item.setProperty('Fanart_Image', fanart_file)
+    dir_item.setArt({ 'fanart': fanart_file })
     return xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = dir_url, listitem = dir_item, isFolder = True)
 
 
 def addLink(action = "", title = "", url = "", thumbnail = "", video_info = {}, show_fanart = False):
-    """This function adds a video link entry into the XBMC GUI throught the API."""
+    """This function adds a video link entry into the KODI GUI throught the API"""
     log("addLink action = [" + action + "] title = [" + title + "] url = [" + url + "] thumbnail = [" + thumbnail + "]")
 
-    link_url = '%s?action=%s&url=%s' % (sys.argv[0], action, urllib.quote_plus(url))
-    link_item = xbmcgui.ListItem(title, iconImage = "DefaultVideo.png", thumbnailImage = thumbnail)
+    link_url = '%s?action=%s&url=%s' % (sys.argv[0], action, quote_plus(url))
+    link_item = xbmcgui.ListItem(title)
     video_info['Title'] = title
     link_item.setInfo(type = "Video", infoLabels = video_info)
     link_item.setProperty('IsPlayable', 'true')
-    if show_fanart:
-        link_item.setProperty('Fanart_Image', thumbnail)
-    else:
-        link_item.setProperty('Fanart_Image', fanart_file)
+    link_item.setArt({
+        'thumb': thumbnail,
+        'fanart': thumbnail if show_fanart else fanart_file,
+        })
     return xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = link_url, listitem = link_item, isFolder = False)
 
 
@@ -188,9 +196,9 @@ def close_dir(pluginhandle, succeeded=True, updateListing=False, cacheToDisc=Tru
 
 
 def showWarning(message):
-    """This funtion shows a popup window with a notices message through the XBMC GUI during 6 secs."""
+    """This funtion shows a popup window with a notices message through the KODI GUI during 6 secs."""
     log("showWarning message: %s" % message)
-    xbmc.executebuiltin('XBMC.Notification(Info:,' + message + '!,6000)')
+    xbmcgui.Dialog().notification('Info:', message, time=6000)
 
 
 def play_resolved_url(pluginhandle= "", url = ""):
